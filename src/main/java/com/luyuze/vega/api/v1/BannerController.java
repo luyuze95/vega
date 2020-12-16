babd68c124968b545c59e8f12dde625e418f77fd
luyuze <627668587@qq.com>
2020-12-16 15:51:37 +0800

    init project

---
+++ src/main/java/com/luyuze/vega/api/v1/BannerController.java
@@ -0,0 +1,15 @@
+package com.luyuze.vega.api.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("banner")
+public class  BannerController {
+
+    @GetMapping("/hello")
+    public String test() {
+        return "hello,陆宇泽111";
+    }
+}
